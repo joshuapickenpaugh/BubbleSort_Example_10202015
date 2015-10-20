814250bb0afe4028c72a6320cd0bb24b961e31e8
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-20 11:36:57 -0500

    Final Commit

--- BubbleSort_Example_10202015/Program.cs
+++ BubbleSort_Example_10202015/Program.cs
@@ -14,6 +14,49 @@ namespace BubbleSort_Example_10202015
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 34, 123, 12, 76, 29, 54, 87 };
+            Console.WriteLine("Before the bubble sort:");
+            DisplayArrayValues(numbers);
+            Console.WriteLine();
+
+            Console.WriteLine("After the bubble sort:");
+            BubbleSortArrayValues(numbers);
+            DisplayArrayValues(numbers);
+            Console.WriteLine();
+
+            Console.WriteLine("Hit a key to escape program.");
+
+            Console.ReadKey();
+        }
+
+        static void DisplayArrayValues(int[] arrayValues)
+        {
+            for (int i = 0; i < arrayValues.Length; i++)
+            {
+                Console.WriteLine("{0}", arrayValues[i]);
+            }
+            Console.WriteLine();
+        }
+
+        static int[] BubbleSortArrayValues(int[] arrayValues)
+        {
+            bool arrayValuesSwapped;
+            do
+            {
+                arrayValuesSwapped = false;
+                for (int i = 0; i < arrayValues.Length - 1; i++)
+                {
+                    if (arrayValues[i] > arrayValues[i + 1])
+                    {
+                        int x = arrayValues[i + 1];
+                        arrayValues[i + 1] = arrayValues[i];
+                        arrayValues[i] = x;
+                        arrayValuesSwapped = true;
+                    }
+                }
+            } while (arrayValuesSwapped == true);
+
+            return arrayValues;
         }
     }
 }
